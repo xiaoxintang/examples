04495a30f5668e265b8f359363d7cff42996a461
longan <15707971810@163.com>
2022-02-22 16:01:24 +0800

    首页添加其他页面的入口

--- umijs/src/pages/index.tsx
+++ umijs/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import styles from './index.less';
-
+import {history} from 'umi'
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>前端example项目</h1>
+      <button onClick={()=>history.push('/dnd-kit/grid')}>拖拽排序</button>
     </div>
   );
 }
